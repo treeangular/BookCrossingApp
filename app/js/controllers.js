827d89ebd04a2ca910419d2cf1fc3ff7a53b1ae0
Joan De Arcayne <arcayne@gmail.com>
2013-03-13 21:30:10 +0100

    Moved to Angular.js
    Pretty big changes ahead, moved all the code to angular.js,
    so far feeling way better than with the life jacket, I love to have a
    motorboat!

---
+++ app/js/controllers.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/* Controllers */
+
+
+function MyCtrl1() {}
+MyCtrl1.$inject = [];
+
+
+function MyCtrl2() {
+}
+MyCtrl2.$inject = [];
+
+function SignInCtrl() {}
+SignInCtrl.$inject = [];
+
+function SignUpCtrl() { }
+SignUpCtrl.$inject = [];
