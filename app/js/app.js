4a08faaa5ae14b37c486dcb483d6120c61d210c3
Joan De Arcayne <arcayne@gmail.com>
2013-03-17 21:57:06 +0100

    Trying to get parse to work with angular
    Added parse libraries and some functioanlly to sign in and up.
    
    Trying to add it now in a way it makes architectural sense. Debate on
    using Parse js api or an angular one in github getting done . . . long
    way to go!!

--- app/js/app.js
+++ app/js/app.js
@@ -2,13 +2,20 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
+angular.module('bookCrossingApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
    // $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
    // $routeProvider.when('/view2', { templateUrl: 'partials/partial2.html', controller: MyCtrl2 });
     $routeProvider.when('/sign', { templateUrl: 'partials/sign.html', controller: SignCtrl });
     $routeProvider.when('/signIn', { templateUrl: 'partials/signIn.html', controller: SignInCtrl });
-    $routeProvider.when('/signUp', { templateUrl: 'partials/signUp.html', controller: SignUpCtrl });
+    $routeProvider.when('/signUp', { templateUrl: 'partials/signUp.html', controller: SignUpCtrl});
     $routeProvider.when('/main', { templateUrl: 'partials/main.html', controller: MainCtrl });
     $routeProvider.otherwise({ redirectTo: '/sign' });
-  }]);
+  }])/*.    TRY INITIALIZE PARSE?!?!?
+  run((function($rootScope) {
+  });*/
+
+/*
+myApp.run(function ($rootScope, $location, $http, $timeout, FooService) {
+    $rootScope.foo = FooService;
+*/
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,25 +2,109 @@
 
 /* Controllers */
 
-function SignInCtrl() {}
-SignInCtrl.$inject = [];
-
-function SignUpCtrl($scope) {
-
+function SignInCtrl($scope) {
     
-   // $scope.master = {};
-    
-    $scope.signUserUp = function() {
-        //$scope.master = angular.copy(user);
-       // console.write(user);
-        console.write("End of signUserUp");
+    var PARSE_APP_ID = "khg4ef8Mks6oP2AjjVlvYHnoEIzLnsaW7Tb23jow";
+    var PARSE_JS_ID = "8KZKpONdEWQZNBteRkJWCtBks3YxuO55THQhQ7qI";
+
+    function initParse() {
+        Parse.initialize(PARSE_APP_ID, PARSE_JS_ID);
+    }
+
+    //this should be done in starting =>Actually if this fails for us is like being offline
+    //Should be online a $rootScope property so we can check it at any time?!
+    initParse();
+
+    $scope.signInUser = function () {
+        Parse.User.logIn($scope.userEmail, $scope.userPassword, {
+            success: function() {
+                //location(#/main);
+                //$.mobile.changePage("main.html", { transition: "slide" });
+                $scope.$apply(function() {
+                    $scope.registerResult = "Register was succesful for user" + email;
+                });
+            },
+            error: function(error) {
+                //Desired functionallyty
+                /*
+                Show error - Go to mode offline - Go back to login page
+                */
+                $scope.registerResult = "Error: " + error.code + " " + error.message + ". Getting into offline mode => No new user can be added at this moment.";
+                alert("Error: " + error.code + " " + error.message);
+            }
+        });
     };
+   
 
 }
-SignUpCtrl.$inject = [];
 
-function SignCtrl() { }
-SignCtrl.$inject = [];
+
+//SignInCtrl.$inject = [];
+
+function SignUpCtrl($scope) {
+   // $scope.master = {};
+    
+    //this should be done in starting =>Actually if this fails for us is like being offline
+    //Should be online a $rootScope property so we can check it at any time?!
+    initParse();
+
+
+    $scope.signUpUser = function(user) {
+       // $scope.master = angular.copy(user);
+        $scope.registerResult = "Register was succesful for user" + user.email;
+    };
+    
+    $scope.registerNewUser = function () {
+        var newUser = new Parse.User();
+        newUser.save({
+            email: $scope.userEmail,
+            password: $scope.userPassword
+        }, {
+            success: function (registersuccess) {
+                $scope.$apply(function () {
+                    $scope.registerResult = "Register was succesful for user" + email;
+                });
+            },
+            error: function (error) {
+                //Desired functionallyty
+                /*
+                Show error - Go to mode offline - Go back to login page
+                */
+                $scope.registerResult = "Error: " + error.code + " " + error.message + ". Getting into offline mode => No new user can be added at this moment.";
+                alert("Error: " + error.code + " " + error.message);
+            }
+        });
+    };
+
+
+}
+//Dependency injection - http://stackoverflow.com/questions/11847376/angular-js-scope-error
+//SignUpCtrl.$inject = ['$scope'];
+
+function SignCtrl() {
+   /* //Check localStorge for user info.
+    var dbShell = window.openDatabase("BookCrossingApp", 2, "BookCrossingApp", 1000000);
+    //doLog("db was opened");
+    //run transaction to create initial tables
+    dbShell.transaction(setupTable, dbErrorHandler, getEntries);
+
+    function setupTable(tx) {
+        //doLog("Going to create the table if it dosent exist");
+        tx.executeSql("CREATE TABLE IF NOT EXISTS dreams(id INTEGER PRIMARY KEY,title,body,updated)");
+    }
+
+    function dbErrorHandler(err) {
+        //alert("DB Error: " + err.message + "\nCode=" + err.code);
+
+        try {
+            // doLog("dbErrorHandler WebSql Error: " + err.message + "\nCode=" + err.code);
+        } catch (e) {
+            // doLog("dbErrorHandler Parse Error: " + err);
+        }
+    }*/
+
+}
+//SignCtrl.$inject = [];
 
 function MainCtrl() { }
 MainCtrl.$inject = [];
