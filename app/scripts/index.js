3f67cc7be41b829611167bcd675a45283148a5dd
Joan De Arcayne <arcayne@gmail.com>
2013-04-06 21:57:54 +0200

    waiting for PG to be ready

---
+++ app/scripts/index.js
@@ -0,0 +1,14 @@
+var app = {
+    initialize: function() {
+        this.bindEvents();
+    },
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, true);
+    },
+
+    onDeviceReady: function() {
+        angular.element(document).ready(function() {
+            angular.bootstrap(document);
+        });
+    }
+};
