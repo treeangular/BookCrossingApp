134f6335367fec426a9389a363adf7cdbcf76842
Joan De Arcayne <arcayne@gmail.com>
2013-04-06 21:43:30 +0200

    using ng-href for navigation
    ng-click dosent seem to be the way to go . . .

--- app/scripts/controllers/Sign.js
+++ app/scripts/controllers/Sign.js
@@ -2,11 +2,6 @@
 
 BookCrossingApp.controller('SignCtrl', function ($scope, DataService, $location) {
 
-    $scope.ToSignIn = function () {
-          //How do I change to another view now?!!? Locate ??
-          $location.path('/main');
-    };
-
     DataService.isCurrentUser(function (result) {
         if (result) {
             $location.path('/Main');
