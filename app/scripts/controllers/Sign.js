ed01466f8d542190dcfcfcaace98c754905bbf4e
Joan De Arcayne <arcayne@gmail.com>
2013-04-06 21:34:32 +0200

    clicking buttons in device

--- app/scripts/controllers/Sign.js
+++ app/scripts/controllers/Sign.js
@@ -1,6 +1,12 @@
 'use strict';
 
 BookCrossingApp.controller('SignCtrl', function ($scope, DataService, $location) {
+
+    $scope.ToSignIn = function () {
+          //How do I change to another view now?!!? Locate ??
+          $location.path('/main');
+    };
+
     DataService.isCurrentUser(function (result) {
         if (result) {
             $location.path('/Main');
