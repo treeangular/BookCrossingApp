7b18f5f9a0be90b770a594b4128cd96eda974271
Javito Hertfy <javierhertfelder@gmail.com>
2013-04-07 11:42:37 +0200

    AddBook test close to fix it
    Still failing AddBook test but very close to fix it, only call to the
    control left

--- test/spec/controllers/controllers/AddBook.js
+++ test/spec/controllers/controllers/AddBook.js
@@ -3,22 +3,37 @@
 describe('Controller: AddBookCtrl', function() {
 
 //  // load the controller's module
-//  beforeEach(module('BookCrossingApp'));
-//
-//  var AddBookCtrl,
-//    scope;
-//
-//  // Initialize the controller and a mock scope
-//  beforeEach(inject(function($controller) {
-//    scope = {};
-//    AddBookCtrl = $controller('AddBookCtrl', {
-//      $scope: scope
-//    });
-//  }));
-//
-//  it('should attach a list of registrationCode to the scope', function () {
-//      expect(scope.registrationCode != null);
-//  });
+  beforeEach(module('BookCrossingApp'));
+
+
+  var AddBookCtrl, scope, book;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function($controller) {
+    scope = {};
+    book = {title: "fooTitle"};
+    AddBookCtrl = $controller('AddBookCtrl', {
+      $scope: scope
+    });
+  }));
+
+    it('should call Parse Service method', function () {
+        //TODO hev: missing call to our method registerNewBook of the Controller
+        //Visit http://evanhahn.com/how-do-i-jasmine/ to get a clear explanation about jasmine!!
+        //We need to get the injector from angular
+        var $injector = angular.injector([ 'DataServices' ]);
+        //We get the service from the injector that we have called
+        var mockService = $injector.get( 'ParseService' );
+        //With this call we SPY the method registerBook of our mockservice
+
+        AddBookCtrl.registerNewBook(book);
+        spyOn(mockService, "registerBook");
+        //we have to make sure that the register book have been called after the call of our Controller
+
+        expect(mockService.registerBook).toHaveBeenCalled();
+        //Dummy test
+        expect(true).toEqual(true);
+    });
     it('Dummy test', function () {
         expect(true).toBe(true);
     });
