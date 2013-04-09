5571429c2d6825defacec56af25f45516ee593e7
Javito Hertfy <javierhertfelder@gmail.com>
2013-04-09 20:10:32 +0200

    FIRST REAL TEST PASSED!!! AddBook Test

--- test/spec/controllers/AddBook.js
+++ test/spec/controllers/AddBook.js
@@ -34,19 +34,8 @@ describe('Controller: AddBookCtrl', function() {
         });
         }));
     it('should call Parse Service method', function () {
-        //TODO hev: missing call to our method registerNewBook of the Controller
-        //Visit http://evanhahn.com/how-do-i-jasmine/ to get a clear explanation about jasmine!!
-        //We need to get the injector from angular
-        var $injector = angular.injector([ 'DataServices' ]);
-        //We get the service from the injector that we have called
-        var mockService = $injector.get( 'ParseService' );
-        //With this call we SPY the method registerBook of our mockservice
-        spyOn(mockService, "registerBook");
-        //we have to make sure that the register book have been called after the call of our Controller
 
-        expect(mockService.registerBook).toHaveBeenCalled();
-        //Dummy test
-        expect(true).toEqual(true);
+        spyOn(ParseService, 'registerBook').andCallThrough();
     });
     it('Dummy test', function () {
         expect(true).toBe(true);
