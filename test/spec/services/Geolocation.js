f0387280f5264ac735069a3c800f3ba51b07e7ae
Javito Hertfy <javierhertfelder@gmail.com>
2013-04-09 23:43:23 +0200

    Added new test for Cordova Geolocalization

--- test/spec/services/Geolocation.js
+++ test/spec/services/Geolocation.js
@@ -3,7 +3,7 @@
 describe('Service: Geolocation', function () {
 
   // load the service's module
-  beforeEach(module('BookCrossingAppApp'));
+  beforeEach(module('BookCrossingApp'));
 
   // instantiate service
   var Geolocation;
