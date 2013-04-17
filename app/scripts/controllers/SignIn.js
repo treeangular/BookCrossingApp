715ceb832291b84b83a9997513cb1a5e337d958e
Marc Canals Giraut <dirkom@gmail.com>
2013-04-17 19:21:26 +0200

    add notification message in signIn screen

--- app/scripts/controllers/SignIn.js
+++ app/scripts/controllers/SignIn.js
@@ -1,18 +1,26 @@
 'use strict';
 
 BookCrossingApp.controller('SignInCtrl', function ($scope, DataService, $location) {
+    $scope.isVisible = false;
+	$scope.close = function() {
+                     $scope.isVisible = false;
+                };
+				
     $scope.signInUser = function (user) {
         DataService.signIn(user.Email, user.Password, function (result) {
             //How do I change to another view now?!!? Locate ?? 
             $scope.$apply(function () {
                 $scope.registerResult = result ? "Success" : "Failed";
+				$scope.isVisible = false;
 
                 if (result) {
                     $scope.registerResult = "Success";
                     $location.path('/Main');
                 } else {
                     $scope.registerResult = "Fail!";
-                    $location.path('/');
+					$scope.ErrorMessage = "User or password invalid!";
+					$scope.isVisible = true;
+					$location.path('/');
                 }
 
             });
