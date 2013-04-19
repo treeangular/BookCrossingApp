31039688509726b88d5acd096fb98507b4a6b5ec
Marc Canals Giraut <dirkom@gmail.com>
2013-04-19 17:57:51 +0200

    refactoring css

--- app/scripts/controllers/signIn.js
+++ app/scripts/controllers/signIn.js
@@ -1,8 +1,6 @@
 'use strict';
 
-
-BookCrossingApp.controller('SignInCtrl', function ($scope, DataService, $location, facebookService) {	
-    			
+BookCrossingApp.controller('SignInCtrl', function ($scope, DataService, $location, facebookService) {				
     $scope.signInUser = function (user) {
         DataService.signIn(user.Email, user.Password, function (result) {
             //How do I change to another view now?!!? Locate ?? 
