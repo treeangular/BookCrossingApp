0c0da179e23e305be96eb4d35dca0f1db36e80c6
Joan De Arcayne <arcayne@gmail.com>
2013-04-19 14:51:48 +0200

    polishing up sign up details

--- app/scripts/controllers/signUp.js
+++ app/scripts/controllers/signUp.js
@@ -3,14 +3,10 @@
 BookCrossingApp.controller('SignUpCtrl', function ($scope, DataService, $location) {
     $scope.registerNewUser = function (user) {
 
-        var isSuccess;
-
         DataService.registerNewUser(user, function (isResult, result) {
-            //How do I change to another view now?!!? Locate ?? 
-            $scope.$apply(function () {
-                isSuccess = isResult ? true : false;
 
-                if (isSuccess)
+            $scope.$apply(function () {
+                if (isResult)
                 {
                     //$scope.registerResult = "Success";
                     $location.path('/SignUpDetails');
@@ -18,12 +14,9 @@ BookCrossingApp.controller('SignUpCtrl', function ($scope, DataService, $locatio
                 else
                 {
                     //Show notification window with error!
+                    console.log("Error: " + error.code + " " + error.message);
                 }
             });
         });
-
-        console.log(isSuccess);
-
-
     };
 });
--- app/scripts/controllers/signUpDetails.js
+++ app/scripts/controllers/signUpDetails.js
@@ -2,9 +2,23 @@
 
 angular.module('BookCrossingApp')
   .controller('SignUpDetailsCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+
+        $scope.updateUserProfile = function (user) {
+
+            DataService.updateUserProfile(user, function (isResult, result) {
+
+                $scope.$apply(function () {
+                    if (isResult)
+                    {
+                        //$scope.registerResult = "Success";
+                        $location.path('/Main');
+                    }
+                    else
+                    {
+                        //Show notification window with error!
+                        console.log("Error: " + error.code + " " + error.message);
+                    }
+                });
+            });
+        }
+});
