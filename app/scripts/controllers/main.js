b3079ea7895d65aa89c7564337b0da2ea9b8e1ea
Marc Canals Giraut <dirkom@gmail.com>
2013-04-27 14:01:45 +0200

    Some chanes in the Home page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,14 +2,74 @@
 
 BookCrossingApp.controller('MainCtrl', function ($scope, dataService, geolocationService) {
     // Call the service and fetch the list of signatures that match the given petition ID
+	$scope.title = 'BookCrossingApp';
+	$scope.subPage='views/home.html';
+	$scope.home='current flip';
+	$scope.myLibrary='flip';
+	$scope.settings='flip';
+	$scope.addBook='flip';
+	$scope.addZobc='flip';
+	
+	$scope.subviewchange = function(page)
+    {
+		switch(page)
+		{
+			case 'views/settings.html':
+				$scope.title = 'Settings';
+				$scope.leftButtonName = null;
+				$scope.rightButtonName = null;
+				$scope.home='flip';
+				$scope.myLibrary='flip';
+				$scope.settings='current flip';
+				$scope.addBook='flip';
+				$scope.addZobc='flip';
+				break;
+			case 'views/addBook.html':
+				$scope.title = 'Register Book';
+				$scope.leftButtonName = null;
+				$scope.rightButtonName = null
+				$scope.home='flip';
+				$scope.myLibrary='flip';
+				$scope.settings='flip';
+				$scope.addBook='current flip';
+				$scope.addZobc='flip';
+				break;
+			case 'views/addZobc.html':
+				$scope.title = 'Add OBCZ';
+				$scope.leftButtonName = null;
+				$scope.rightButtonName = null
+				$scope.home='flip';
+				$scope.myLibrary='flip';
+				$scope.settings='flip';
+				$scope.addBook='flip';
+				$scope.addZobc='current flip';
+				break;
+			case 'views/myLibrary.html':
+				$scope.title = 'My Library';
+				$scope.leftButtonName = null;
+				$scope.rightButtonName = null
+				$scope.home='flip';
+				$scope.myLibrary='current flip';
+				$scope.settings='flip';
+				$scope.addBook='flip';
+				$scope.addZobc='flip';
+				break;				
+			default:				
+				$scope.title = 'BookCrossingApp';			
+				$scope.leftButtonName = null;
+				$scope.rightButtonName = 'Map';
+				$scope.subPage='views/home.html';
+				$scope.home='current flip';
+				$scope.myLibrary='flip';
+				$scope.settings='flip';
+				$scope.addBook='flip';
+				$scope.addZobc='flip';
+		}
+		$scope.subPage = page;
+    };
+	
     dataService.getWholeActions(function (results) {
         $scope.$apply(function () {
-
-			$scope.title = 'BookCrossingApp';
-			$scope.leftButtonName = 'Back';
-			$scope.rightButtonName = 'Filter';
-			$scope.subPage='views/home.html';
-						
             //if the object is complex 
             $scope.actionList = results;
             //Todo hev: try to understand 
