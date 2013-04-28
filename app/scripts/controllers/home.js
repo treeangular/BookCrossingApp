bbc17d39eb4d95a8d3fdc434a504937c1ed32a9b
Marc Canals Giraut <dirkom@gmail.com>
2013-04-28 20:50:42 +0200

    trying to figure out why the main is not working...

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -6,10 +6,10 @@ BookCrossingApp.controller('HomeCtrl', function($scope) {
 		{type:'book', title:'Lords of the Rings', content: 'has been released arround you by PersonName'
 		, image:'styles/img/books/lord_of_the_rings.jpg', time:'15 min'},
 		{type:'ad', image:'styles/img/ads/amazon.jpg'},
+		{type:'book', title:'A Clockwork orange', content: 'has been released arround you by PersonName'
+		, image:'styles/img/books/a_clockwork_orange.jpg', time:'1 min'},
         {type:'book', title:'1Q84',  content: 'has been commented by PersonName'
 		, image:'styles/img/books/1q84.jpg', time:'5 min'},
-		{type:'book', title:'Lords of the Rings',  content: 'has been hunted by PersonName'
-		, image:'styles/img/books/lord_of_the_rings.jpg' , time:'10 hours'},
 		{type:'ad', image:'styles/img/ads/mcdonalls.jpg'}
     ];
 
