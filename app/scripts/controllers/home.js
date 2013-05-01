12cb882ceeeb759afb96c990f5744501f7fe86ef
Marc Canals Giraut <dirkom@gmail.com>
2013-05-01 17:48:29 +0200

    get books from the server

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -1,5 +1,7 @@
 'use strict';
-BookCrossingApp.controller('HomeCtrl', function($scope) {
+BookCrossingApp.controller('HomeCtrl', function($scope, dataService) {
+
+	//Some test books. TODO: Remove them when we
 	$scope.alerts = [
         {type:'book', title:'A Clockwork orange', content: 'has been released arround you by PersonName'
 		, image:'styles/img/books/a_clockwork_orange.jpg', time:'1 min'},
@@ -12,6 +14,43 @@ BookCrossingApp.controller('HomeCtrl', function($scope) {
 		, image:'styles/img/books/1q84.jpg', time:'5 min'},
 		{type:'ad', image:'styles/img/ads/mcdonalls.jpg'}
     ];
+	
+	dataService.getWholeActions(function (results) {
+        $scope.$apply(function () {
+            //TODO: Load only first page and then use paging in the NextPage function!
+            $scope.actionList = results;
+			for (var i=0;i<results.length;i++)
+			{
+			var action = results[i];
+			var book = action.get('bookPointer');
+			var actionType = action.get('actionTypePointer');
+			var user = action.get('userPointer');
+			
+			var title = "not defined";
+			var description = "not defined";
+			var username = "not defined";
+			
+			if (book != null)
+				title = book.get('title');
+			if (actionType != null)
+				description = actionType.get('Description');
+			if (user != null)
+				username = user.get('username');
+			
+			
+			$scope.newalert = {
+				type:'book', 
+				title: title, 
+				content: description,
+				image:'styles/img/books/lord_of_the_rings.jpg',
+				user: username,
+				time:'2 days'
+			};
+			$scope.alerts.push($scope.newalert);
+            console.log("Getting books from database!");
+			}
+        });
+    });
 
   $scope.busy = false;
 
@@ -19,17 +58,7 @@ BookCrossingApp.controller('HomeCtrl', function($scope) {
     if ($scope.busy) return;
     $scope.busy = true;
     
-	$scope.newalert = {
-		type:'book', 
-		title:'Lords of the Rings', 
-		content: 'has been released arround you by PersonName', 
-		image:'styles/img/books/lord_of_the_rings.jpg', 
-		time:'2 days'
-	};
-		   
-	for(var i = 1; i <= 3; i++) {
-      $scope.alerts.push($scope.newalert);
-    }
+	//TODO: Add here paging!
     
 	$scope.busy = false;
   };
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-BookCrossingApp.controller('MainCtrl', function ($scope, dataService, geolocationService) {
+BookCrossingApp.controller('MainCtrl', function ($scope) {
     // Call the service and fetch the list of signatures that match the given petition ID
 	$scope.title = 'BookCrossingApp';
 	$scope.leftButtonName = null;
@@ -69,26 +69,5 @@ BookCrossingApp.controller('MainCtrl', function ($scope, dataService, geolocatio
 		}
 		$scope.subPage = page;
     };
-	
-    dataService.getWholeActions(function (results) {
-        $scope.$apply(function () {
-            //if the object is complex 
-            $scope.actionList = results;
-            //Todo hev: try to understand 
-            //$scope.actionList = results.models;
-            /*
-            geolocationService.getCurrentPosition(function (position) {
-                alert('Latitude: '              + position.coords.latitude          + '\n' +
-                    'Longitude: '             + position.coords.longitude         + '\n' +
-                    'Altitude: '              + position.coords.altitude          + '\n' +
-                    'Accuracy: '              + position.coords.accuracy          + '\n' +
-                    'Altitude Accuracy: '     + position.coords.altitudeAccuracy  + '\n' +
-                    'Heading: '               + position.coords.heading           + '\n' +
-                    'Speed: '                 + position.coords.speed             + '\n' +
-                    'Timestamp: '             + position.timestamp                + '\n');
-            }); */
-
-            console.log("Are we getting here more than once?!?");
-        });
-    });
+    
 });
