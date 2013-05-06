3648fb9341dd97376b07da8a1d48399fd28a4d6f
Marc Canals Giraut <dirkom@gmail.com>
2013-05-06 12:29:03 +0200

    add mock map image

--- app/scripts/controllers/releaseBook.js
+++ app/scripts/controllers/releaseBook.js
@@ -1,6 +1,15 @@
 'use strict';
 BookCrossingApp.controller('ReleaseBookCtrl', function($scope, dataService) {
 
+    //TODO: Get Zobc around the current location
+    $scope.zobcList = [
+        {id:'0', name:'OBZC around you'},
+        {id:'112121', name:'Bar Cosmo'},
+        {id:'222121', name:'Cafeteria Lola'},
+        {id:'112221', name:'Libreria Central'},
+        {id:'132121', name:'Biblioteca Universidad Barcelona'}
+    ];
+
     $scope.release = function () {
         //TODO: Use google maps to find the location
     };
@@ -8,5 +17,4 @@ BookCrossingApp.controller('ReleaseBookCtrl', function($scope, dataService) {
     $scope.findLocation = function () {
         //TODO: Use google maps to find the location
     };
-
 });
