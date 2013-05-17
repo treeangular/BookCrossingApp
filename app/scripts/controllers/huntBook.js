60e6eb49bb2e75b007f62868d8920f8ee65b2592
Joan De Arcayne <arcayne@gmail.com>
2013-05-17 18:00:58 +0200

    Hunt book
    Release and hunt works well - update the Book status in the cloud code
    for the book dosen't.
    Weirdness when more than one action is getting saved. Carefull with the
    loops we might be having.

--- app/scripts/controllers/huntBook.js
+++ app/scripts/controllers/huntBook.js
@@ -1,7 +1,19 @@
 'use strict';
 
-BookCrossingApp.controller('HuntBookCtrl', function ($scope) {
-    $scope.huntBook = function (barcode) {
-        //TODO: Save book hunted action!
+BookCrossingApp.controller('HuntBookCtrl', function ($scope,dataService,$rootScope) {
+    $scope.huntBook = function (book) {
+
+                dataService.huntBook(book.registrationId,function(isSuccess)
+                {
+                    if(isSuccess)
+                    {
+                        //$rootScope.
+                        $scope.goTo('views/book.html');
+                    }
+                    else
+                    {
+                        $rootScope.ErrorMessage = "Oops . . . Please try to hunt it again in a few seconds.";
+                    }
+                });
     };
 });
