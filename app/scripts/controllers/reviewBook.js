35a21f5c8408e328d120dd37248789c90d258181
Javito Hertfy <javierhertfelder@gmail.com>
2013-07-01 20:16:32 +0200

    Binded ReviewList and Review Book without like

--- app/scripts/controllers/reviewBook.js
+++ app/scripts/controllers/reviewBook.js
@@ -1,12 +1,50 @@
-BookCrossingApp.controller('ReviewsBookCtrl', function ($scope, $rootScope, dataService) {
+BookCrossingApp.controller('ReviewsBookCtrl', function ($scope, $rootScope, dataService, $q) {
   $scope.review = {
-      rate:0,
-      content: ""
+      rating:0,
+      reviewText: ""
   }
 
-  $scope.sendReview = function () {
-    //TODO: Save review (rate + content)
-    $scope.goTo('views/bookDetails.html');
+    function saveReview(review)
+    {
+
+        $rootScope.$broadcast(loadingRequestConst.Start);
+        var deferred = $q.defer();
+        review.book = $scope.selectedBook;
+        dataService.addReviewToBook(review, function (isSuccess, result) {
+
+            $scope.$apply(function(){
+                if(isSuccess)
+                {
+                    deferred.resolve(result);
+                }
+                else
+                {
+                    deferred.reject(result);
+                }
+            });
+
+        });
+
+        return deferred.promise;
+
+    }
+
+  $scope.sendReview = function (review) {
+
+      $scope.clicked = true;
+      var promise = saveReview(review)
+      promise.then(function(review) {
+
+          $rootScope.$broadcast(loadingRequestConst.Stop);
+          $scope.clicked = false;
+          $scope.goTo('views/bookDetails.html');
+      }, function(reason) {
+
+          $rootScope.TypeNotification = ErrorConst.TypeNotificationError;
+          $rootScope.MessageNotification = reason;
+      });
+
+
   }
 
 });
