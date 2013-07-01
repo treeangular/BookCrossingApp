0b41275e4a4b81474ff019eaf0f8b1b3db98476d
Javito Hertfy <javierhertfelder@gmail.com>
2013-07-01 23:49:50 +0200

    logout fb

--- app/scripts/controllers/settings.js
+++ app/scripts/controllers/settings.js
@@ -1,12 +1,16 @@
 'use strict';
 
-BookCrossingApp.controller('SettingsCtrl', function ($scope, dataService, $location) {
+BookCrossingApp.controller('SettingsCtrl', function ($scope, dataService, $location, facebookService, $rootScope) {
     $scope.SignOut = function()
     {
         console.log("Going to log out from ctrl");
 
         //Call DataService signOut function
         dataService.signOut();
+        alert($rootScope.currentUser.get("fbId"))
+        alert("2"+$rootScope.currentUser.fbId)
+        if($rootScope.currentUser.get("fbId") != undefined)
+        facebookService.logout();
 
         //Back to beginning
         $location.path('/');
