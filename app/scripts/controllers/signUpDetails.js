1644f5186bb1e42ae1f6d6a7ca133cef044e51f9
Joan De Arcayne <arcayne@gmail.com>
2013-07-11 08:56:17 -0400

    new version

--- app/scripts/controllers/signUpDetails.js
+++ app/scripts/controllers/signUpDetails.js
@@ -26,18 +26,6 @@ angular.module('BookCrossingApp')
             }
         };
 
-        $scope.$watch('myPicture', function(value) {
-            if(value) {
-                $scope.myPicture = value;
-            }
-        }, true);
-
-        $scope.$watch('profilePhotoFileUpload', function(value) {
-            if(value) {
-                $scope.myPicture = value;
-            }
-        }, true);
-
         dataService.getCurrentUser(function(currentUser){
             var profilePhoto = "styles/img/user.png";
 
@@ -62,61 +50,7 @@ angular.module('BookCrossingApp')
             $scope.selectSex($scope.user.gender);
          });
 
-        $scope.uploadProfilePicView = function (value)
-        {
-
-            $scope.myPicture = value.value;
-        }
-
         $scope.updateUserProfile = function (user) {
-
-            //var image = document.getElementById('profilePhotoFileUpload');
-            var fileUploadControl = $("#profilePhotoFileUpload")[0];
-            if (fileUploadControl.files.length > 0) {
-                var file = fileUploadControl.files[0];
-                var name = "photo.jpg";
-
-                var parseFile = new Parse.File(name, file);
-
-                parseFile.save();
-
-                var currentUser = Parse.User.current();
-
-                //currentUser.set("myPicture",parseFile._url);
-                currentUser.set("myFile",parseFile);
-                //currentUser.save();
-
-                /*parseFile.save().then(function() {
-                    // The file has been saved to Parse.
-                    console.log("Time to associate file with and parse object");
-                    var currentUser = Parse.User.current();
-
-                    currentUser.set("myPicture",parseFile._url);
-                    currentUser.set("myFile",parseFile);
-
-                    currentUser.save();
-
-                }, function(error) {
-                    // The file either could not be read, or could not be saved to Parse.
-                    console.log("Error: " + error.code + " " + error.message)
-                });*/
-
-                //var file = new Parse.File("userPicture.JPEG", { base64: image });
-
-                /*dataService.uploadPicture(parseFile, function (result) {
-
-                    $scope.$apply(function () {
-                        if (result)
-                        {
-
-                        }
-                        else
-                        {
-                            $rootScope.TypeNotification = "errormessage";
-                            $rootScope.MessageNotification = result.message;
-                        }
-                    });
-                });
 
                 dataService.updateUserProfile(user, function (isResult, result) {
 
@@ -131,27 +65,33 @@ angular.module('BookCrossingApp')
                             $rootScope.MessageNotification = result.message;
                         }
                     });
-                });*/
-            }
+                });
         }
-
-        var imageUriToLoad;
 
         $scope.getPicture = function(){
 
-            navigator.camera.getPicture(onSuccess, onFail,
-                //Options => http://docs.phonegap.com/en/2.6.0/cordova_camera_camera.md.html#Camera
-                { quality: 50,
-                    //destinationType:Camera.DestinationType.FILE_URI,
-                    destinationType:Camera.DestinationType.DATA_URL,
-                    encodingType: Camera.EncodingType.JPEG,
-                    sourceType:navigator.camera.PictureSourceType.PHOTOLIBRARY,
-                    //sourceType : Camera.PictureSourceType.PHOTOLIBRARY ,//CAMERA,
-                    targetWidth: 100,
-                    targetHeight: 100
+//            navigator.camera.getPicture(onSuccess, onFail,
+//                //Options => http://docs.phonegap.com/en/2.6.0/cordova_camera_camera.md.html#Camera
+//                { quality: 50,
+//                    //destinationType:Camera.DestinationType.FILE_URI,
+//                    destinationType:Camera.DestinationType.DATA_URL,
+//                    encodingType: Camera.EncodingType.JPEG,
+//                    sourceType:navigator.camera.PictureSourceType.PHOTOLIBRARY,
+//                    //sourceType : Camera.PictureSourceType.PHOTOLIBRARY ,//CAMERA,
+//                    targetWidth: 100,
+//                    targetHeight: 100
+//                });
+
+            navigator.camera.getPicture(gotPic, failHandler,
+                {quality:50,
+                 destinationType:Camera.DestinationType.FILE_URI,
+                 sourceType:navigator.camera.PictureSourceType.PHOTOLIBRARY
                 });
 
-            function onSuccess(data) {
+            function gotPic(data) {
+
+                $scope.myPicture = data;
+
                 window.resolveLocalFileSystemURI(data, function(entry) {
 
                     var reader = new FileReader();
