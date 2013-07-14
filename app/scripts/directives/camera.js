e521418e1bdc31843caa4c4886c98f0537811416
Joan De Arcayne <arcayne@gmail.com>
2013-07-14 18:07:46 +0200

    Rearrange Pic functionally for abstraction & architectural guidelines

--- app/scripts/directives/camera.js
+++ app/scripts/directives/camera.js
@@ -18,12 +18,53 @@ BookCrossingApp.directive('bcaCamera', function () {
                             targetHeight: 100
                         });
                     function onSuccess(imageData) {
-                        var image = document.getElementById('myPicture');
-                        image.src = "data:image/jpeg;base64," + imageData;
-                        alert("Hola!!");
-                        $scope.$apply(function() {
+//                        var image = document.getElementById('myPicture');
+//                        image.src = "data:image/jpeg;base64," + imageData;
+//                        alert("Hola!!");
+
+                            window.resolveLocalFileSystemURI(imageData, function(entry) {
+
+                                var reader = new FileReader();
+
+                                reader.onloadend = function(evt) {
+                                    console.log('read onloderend');
+                                    console.log(JSON.stringify(evt.target));
+                                    console.log(evt.target.result);
+                                    var byteArray = new Uint8Array(evt.target.result);
+                                    var output = new Array( byteArray.length );
+                                    var i = 0;
+                                    var n = output.length;
+                                    while( i < n ) {
+                                        output[i] = byteArray[i];
+                                        i++;
+                                    }
+                                    //return output
+                                    $scope.$apply(function() {
+                                        ctrl.$setViewValue(output);
+                                    });
+
+                                }
+
+                                reader.onerror = function(evt) {
+                                    console.log('read error');
+                                    console.log(JSON.stringify(evt));
+                                }
+
+                                console.log('pre read');
+
+                                entry.file(function(s) {
+                                    reader.readAsArrayBuffer(s);
+                                }, function(e) {
+                                    console.log('ee');
+                                });
+
+                                //reader.readAsArrayBuffer(entry.file(function(s) { console.log('ss');}, function(e) { console.log('e');});
+                                console.log('fired off the read...');
+                            });
+
+                       /* $scope.$apply(function() {
                             ctrl.$setViewValue(image.src);
-                        });
+                        });*/
                     }
 
                     function onFail(message) {
