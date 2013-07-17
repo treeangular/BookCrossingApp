d0eaf8508f219557c2ef9e098dc18a40226692c0
Javito Hertfy <javierhertfelder@gmail.com>
2013-07-17 22:23:34 +0200

    Added kilometers.. not working

--- app/scripts/vendor/parse/bcCloudCode/cloud/main.js
+++ app/scripts/vendor/parse/bcCloudCode/cloud/main.js
@@ -56,7 +56,7 @@ Parse.Cloud.beforeSave("Book", function(request, response) {
             bookCounterToIncerement = "hunted";
             break;
         default:
-            //Do nothing
+        //Do nothing
     }
 
     request.object.increment(bookCounterToIncerement);
@@ -281,36 +281,54 @@ Parse.Cloud.afterSave("Book", function (request) {
             query.equalTo("book", request.object);
             query.descending("createdAt");
 
-
             query.find({
                 success: function (trackingq) {
 
                     if(trackingq.length > 0)
                     {
+                        alert("success!!");
+                        console.log(trackingq[0].get("releasedAt"))
+                        console.log(request.object.get("releasedAt"))
 
-                    updateBookKilometers(request, trackingq[0].get("releasedAt"), request.object.get("releasedAt"));
+                        var point1 = trackingq[0].get("releasedAt");
+                        var point2 = request.object.get("releasedAt");
 
-                    console.log("It is a release going to create the Tracking record ");
-                    }
+                        console.log(trackingq.length);
 
-                    console.log("no tracking yet")
-                    //Set the new tracking record
-                    tracking.set("releasedBy", request.object.get("ownedBy"));
-                    tracking.set("releasedAt", request.object.get("releasedAt"));
-                    tracking.set("releasedAtDescription", request.object.get("releasedAtDescription"));
-                    tracking.set("book", request.object);
-
-                    tracking.save(null, {
-                        success: function (tracking) {
-                            // The object was saved successfully.
-                            console.log("Tracking record was saved successfully." );
-                        },
-                        error: function (error) {
-                            // The save failed.
-                            // error is a Parse.Error with an error code and description.
-                            console.log("Error tracking.save: " + error.code + " " + error.message);
+                        if(request.object.get("kilometers")== undefined)
+                        {
+                            console.log("undefined!!");
+                            console.log(point1.kilometersTo(point2));
+                             request.object.increment("kilometers", point1.kilometersTo(point2));
                         }
-                    });
+                        else
+                        {
+                            console.log("defined!!");
+                            console.log(numberOfKilometersSoFar);
+                            var numberOfKilometersSoFar = request.object.get("kilometers") + point1.kilometersTo(point2);
+                            console.log(numberOfKilometersSoFar);
+                            request.object.increment("kilometers", numberOfKilometersSoFar);
+                        }
+
+                        request.object.save({
+                            success: function(myObject) {
+
+                                console.log("book saved:" + myObject);
+                                saveTracking(request, tracking);
+
+                            },
+                            error: function(myObject, error) {
+
+                                console.error(error);
+                            }
+                        });
+
+                        console.log("It is a release going to create the Tracking record ");
+                    }
+                    else
+                    {
+                        saveTracking(request, tracking);
+                    }
 
                 },
                 error: function (error) {
@@ -326,8 +344,8 @@ Parse.Cloud.afterSave("Book", function (request) {
         //Send email after registration
         if(actionTypeId == ActionTypesConst.Registered)
         {
-        var Mandrill = require('mandrill');
-        Mandrill.initialize(mandrillApiKey);
+            var Mandrill = require('mandrill');
+            Mandrill.initialize(mandrillApiKey);
             //var user = request.user;
             var nick = request.user.get("nick");
             var email = request.user.get("email");
@@ -372,44 +390,24 @@ Parse.Cloud.afterSave("Book", function (request) {
     }
 });
 
-function updateBookKilometers(book,point1, point2)
+function saveTracking(request, tracking)
 {
+    console.log("no tracking yet")
+    //Set the new tracking record
+    tracking.set("releasedBy", request.object.get("ownedBy"));
+    tracking.set("releasedAt", request.object.get("releasedAt"));
+    tracking.set("releasedAtDescription", request.object.get("releasedAtDescription"));
+    tracking.set("book", request.object);
 
-    if(book.object.get("kilometers")== undefined)
-    {
-
-        book.object.increment("kilometers", point1.kilometersTo(point2));
-    }
-    else
-    {
-        numberOfKilometersSoFar = book.object.get("kilometers") + point1.kilometersTo(point2);
-        book.object.increment("kilometers", numberOfKilometersSoFar);
-    }
-
-    book.object.fetch({
-        success: function(myObject) {
-
-            myObject.object.save({
-                success: function(httpResponse) {
-
-                    console.log("book kilometers saved sent!");
-                },
-                error: function(httpResponse) {
-
-                    console.error(httpResponse);
-
-                }
-
-            })
+    tracking.save(null, {
+        success: function (tracking) {
+            // The object was saved successfully.
+            console.log("Tracking record was saved successfully." );
         },
-        error: function(myObject, error) {
-
-            console.error(error);
+        error: function (error) {
+            // The save failed.
+            // error is a Parse.Error with an error code and description.
+            console.log("Error tracking.save: " + error.code + " " + error.message);
         }
     });
-
-
-
-
-
 }
