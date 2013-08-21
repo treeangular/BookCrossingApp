76b43b950eef7735b83012c064cf552254a39f1e
Javier Hertfelder <javierhertfelder@gmail.com>
2013-08-21 15:02:59 +0200

    added directive

--- app/scripts/directives/ngTap.js
+++ app/scripts/directives/ngTap.js
@@ -1,23 +1,28 @@
 BookCrossingApp.directive("ngTap", function() {
-    return function($scope, $element, $attributes) {
-        var tapped;
-        tapped = false;
-        $element.bind("click", function() {
-            if (!tapped) {
-                return $scope.$apply($attributes["ngTap"]);
-            }
-        });
-        $element.bind("touchstart", function(event) {
-            return tapped = true;
-        });
-        $element.bind("touchmove", function(event) {
+
+    return {
+        restrict: "A",
+        link: function($scope, $element, $attributes) {
+
+            var tapped;
             tapped = false;
-            return event.stopImmediatePropagation();
-        });
-        return $element.bind("touchend", function() {
-            if (tapped) {
-                return $scope.$apply($attributes["ngTap"]);
-            }
-        });
+            $element.bind("click", function() {
+                if (!tapped) {
+                    return $scope.$apply($attributes["ngTap"]);
+                }
+            });
+            $element.bind("touchstart", function(event) {
+                return tapped = true;
+            });
+            $element.bind("touchmove", function(event) {
+                tapped = false;
+                return event.stopImmediatePropagation();
+            });
+            return $element.bind("touchend", function() {
+                if (tapped) {
+                    return $scope.$apply($attributes["ngTap"]);
+                }
+            });
+        }
     };
 });
