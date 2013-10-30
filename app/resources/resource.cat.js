bd2ab7b7ea54cc434a7c0175f5fd7f4744a0788b
Joan De Arcayne <arcayne@gmail.com>
2013-10-30 18:08:20 +0100

    Added Catalan support

---
+++ app/resources/resource.cat.js
@@ -0,0 +1,527 @@
+[
+    {
+        "key": "_SignIn_",
+        "value": "Entra",
+        "description": "Sign In button"
+    },
+    {
+        "key": "_SignUp_",
+        "value": "Registrat",
+        "description": "Sign In button"
+    },
+    {
+        "key": "_TakeTour_",
+        "value": "Take a tour",
+        "description": "Take a tour link to a youtube video"
+    },
+    {
+        "key": "_Connect_",
+        "value": "Conecta amb Facebook",
+        "description": "Connect button on the application"
+    },
+    {
+        "key": "_CityNotFound_",
+        "value": "No s'ha trobat la ciutat",
+        "description": "City not found"
+    },
+    {
+        "key": "_Cancel_",
+        "value": "Cancela",
+        "description": "Cancel string for all the app"
+    },
+    {
+        "key": "_ConnectFacebook_",
+        "value": "Conecta amb Facebook",
+        "description": "Connect to FB button"
+    },
+    {
+        "key": "_Email_",
+        "value": "Email",
+        "description": "Email placeholder"
+    },
+    {
+        "key": "_Password_",
+        "value": "Contrasenya",
+        "description": "Password placeholder"
+    },
+    {
+        "key": "_SeePassword_",
+        "value": "Veure contrasenya",
+        "description": "Checkbox for see the password"
+    },
+    {
+        "key": "_Register_",
+        "value": "Registrat",
+        "description": "Register button"
+    },
+    {
+        "key": "_GenericError_",
+        "value": "Disculpa, an error occurred, try it later",
+        "description": "Generic Error"
+    },
+    {
+        "key": "_UserNotRegisteredCorrectly_",
+        "value": "Disculpa, user not registered",
+        "description": "User not registered"
+    },
+    {
+        "key": "_UserNotUpdatedCorrectly_",
+        "value": "Disculpa, user was not updated",
+        "description": "User not registered"
+    },
+    {
+        "key": "_UserNotFound_",
+        "value": "Disculpa, no 'sha trbat l'usuari",
+        "description": "User not found"
+    },
+    {
+        "key": "_BookAlreadyHunted_",
+        "value": "Disculpa, el llibre ja ha estat caÃ§at",
+        "description": "book is already hunted"
+    },
+    {
+        "key": "_IsbnNotFound_",
+        "value": "Disculpa, no em trobat el ISBN",
+        "description": "Isbn not found"
+    },
+    {
+        "key": "_BookNotFound_",
+        "value": "Disculpa, no em trobat el llibre",
+        "description": "book not found"
+    },
+    {
+        "key": "_UserLoginError_",
+        "value": "Disculpa, error de login",
+        "description": "book not found"
+    },
+    {
+        "key": "_UserNotAuthorized_",
+        "value": "Disculpa, usuari no autoritzat",
+        "description": "book not found"
+    },
+    {
+        "key": "_RegistrationIdError_",
+        "value": "Disculpa, error registrant el llibre",
+        "description": "book not found"
+    },
+    {
+        "key": "_hunted_",
+        "value": "CaÃ§at",
+        "description": "hunted book in home"
+    },
+    {
+        "key": "_released_",
+        "value": "Lliberat",
+        "description": "released book in home"
+    },
+    {
+        "key": "_commented_",
+        "value": "Comentat",
+        "description": "commented book in home"
+    },
+    {
+        "key": "_reviewed_",
+        "value": "OpiniÃ³",
+        "description": "reviewed book"
+    },
+    {
+        "key": "_registered_",
+        "value": "Registrat",
+        "description": "registered book"
+    },
+    {
+        "key": "_lost_",
+        "value": "Perdut",
+        "description": "lost book"
+    },
+    {
+        "key": "_locked_",
+        "value": "Bloquejat",
+        "description": "locked book"
+    },
+    {
+        "key": "_joined_",
+        "value": "Unit",
+        "description": "joined user"
+    },
+    {
+        "key": "_IsbnNotFoundButton_",
+        "value": "No ho trobes per ISBN?",
+        "description": "ISBN not found"
+    },
+    {
+        "key": "_Isbn_",
+        "value": "ISBN, titol, autor",
+        "description": "ISBN"
+    },
+    {
+        "key": "_FindBook_",
+        "value": "Trobar un llibre",
+        "description": "Find book"
+    },
+    {
+        "key": "_ScanBook_",
+        "value": "Scaneja el codi de barres",
+        "description": "Scan Book Barcode"
+    },
+    {
+        "key": "_CheckBookInformation_",
+        "value": "Check book information",
+        "description": "Check book Information"
+    },
+    {
+        "key": "_Title_",
+        "value": "TÃ­tol",
+        "description": "Title"
+    },
+    {
+        "key": "_Description_",
+        "value": "DescripciÃ³",
+        "description": "Description"
+    },
+    {
+        "key": "_AddBookInformation_",
+        "value": "Afegeix informaciÃ³ del llibre",
+        "description": "Add book information"
+    },
+    {
+        "key": "_TakePicture_",
+        "value": "fes una foto",
+        "description": "Take a picture"
+    },
+    {
+        "key": "_SelectPicture_",
+        "value": "Selecciona de la galeria",
+        "description": "Select a picture"
+    },
+    {
+        "key": "_SignUpDetailsHeader_",
+        "value": "Personalitza el teu perfil",
+        "description": "sign up details header"
+    },
+    {
+        "key": "_SaveUpdatedUserProfile_",
+        "value": "Salvar",
+        "description": "Save value for button in sign up details"
+    },
+    {
+        "key": "_SkipSignUpDetails_",
+        "value": "Ara no",
+        "description": "skip saving sign up details"
+    },
+    {
+        "key": "_ReleaseBookRegistrationId_",
+        "value": "Codi de registre",
+        "description": "Placeholder for release book registration id input"
+    },
+    {
+        "key": "_HuntABook_",
+        "value": "CaÃ§a un llibre",
+        "description": "hunt a book"
+    },
+    {
+        "key": "_RegistrationId_",
+        "value": "Codi de Registre",
+        "description": "Registration Code"
+    },
+    {
+        "key": "_ReleaseABook_",
+        "value": "Allibera el llibre",
+        "description": "Release a Book"
+    },
+    {
+        "key": "_NoBooksToRelease_",
+        "value": "No hi han llibres per a lliberar",
+        "description": "No books to release"
+    },
+    {
+        "key": "_SeeMoreAlerts_",
+        "value": "Veure mes alertes",
+        "description": "See more alerts"
+    },
+    {
+        "key": "_AddZobc_",
+        "value": "Afegeix un ZOBC",
+        "description": "See more alerts"
+    },
+    {
+        "key": "_UpdateYourProfile_",
+        "value": "Actualitza el teu perfil",
+        "description": "See more alerts"
+    },
+    {
+        "key": "_SignOut_",
+        "value": "Sortir",
+        "description": "See more alerts"
+    },
+    {
+        "key": "_HelpBookCrossingDefinition_",
+        "value": "App per trobar i compartir llibres amb altra gent arreu del mon.",
+        "description": "App for finding and sharing books with other people around the world."
+    },
+    {
+        "key": "_HelpRegisterBook_",
+        "value": "Registra el llibre",
+        "description": "Register a book"
+    },
+    {
+        "key": "_HelpRegisterBookDescription_",
+        "value": "Registra el teu llibre i copia el codi que rebrÃ s al teu correu.",
+        "description": "Register your book and paste the barcode on it that you will receive in your email."
+    },
+    {
+        "key": "_HelpReleaseBook_",
+        "value": "Allibera un llibre",
+        "description": "Release a book"
+    },
+    {
+        "key": "_HelpReleaseBookDescription_",
+        "value": "Ves fins al lloc on vols deixar el llibre i utilitza la app per alliberar-lo.",
+        "description": "Go to the location where you want to leave the book and use the app to release it."
+    },
+    {
+        "key": "_HelpFollowBook_",
+        "value": "Segueix el teu llibre",
+        "description": "Follow your book"
+    },
+    {
+        "key": "_HelpFollowBookDescription_",
+        "value": "Ves a la teva llibreria i selecciona el llibre que vols veure com ha viatjat, comentaris, opinions d'altres persones d'arreu del mon.",
+        "description": "Go to your library and select the book to see the travel route, comments, and reviews of other people who hunted the book."
+    },
+    {
+        "key": "_HelpFindingBooks_",
+        "value": "Trobar un llibre",
+        "description": "Finding a book"
+    },
+    {
+        "key": "_HelpFindingBooksAroundYou_",
+        "value": "Troba llibres al teu voltant",
+        "description": "Find books around you"
+    },
+    {
+        "key": "_HelpFindingBooksAroundYouDescription_",
+        "value": "Utilitza el mapa per trobar un llibre al teu voltant i agafa'l.",
+        "description": "Use the map to find a book around you and go to pick it up."
+    },
+    {
+        "key": "_HelpHuntBook_",
+        "value": "CaÃ§a un llibre",
+        "description": "Hunt a book"
+    },
+    {
+        "key": "_HelpHuntBookDescription_",
+        "value": "CaÃ§a un llibre amb la app utilitzant el codi de registre que trobarÃ s al llibre.",
+        "description": "Hunt the book with the app using the barcode that you will find in the book."
+    },
+    {
+        "key": "_HelpSeeVideo_",
+        "value": "Veure el video",
+        "description": "See video"
+    },
+    {
+        "key": "_HelpSharingBook_",
+        "value": "Compartint llibres",
+        "description": "Sharing books"
+    },
+    {
+        "key": "_ResultsFound_",
+        "value": "Resultats trobats",
+        "description": "results found"
+    },
+    {
+        "key": "_Publisher_",
+        "value": "Publicador",
+        "description": "Publisher"
+    },
+    {
+        "key": "_PublishingDate_",
+        "value": "data de publicaciÃ³",
+        "description": "Publishing Date"
+    },
+    {
+        "key": "_BookRegisteredSuccessfully_",
+        "value": "Llibre registrat correctament!",
+        "description": "Book registered succesfully"
+    },
+    {
+        "key": "_FollowTheNextSteps_",
+        "value": "Segueix els seguents pasos:",
+        "description": "Book registered succesfully"
+    },
+    {
+        "key": "_PrintABookCrossingAppLabel_",
+        "value": "1- Imprimeix/Escriu/Dibuixa la estiqueta BookCorssingApp",
+        "description": "Book registered succesfully"
+    },
+    {
+        "key": "_WriteYourRegistrationIdInTheLabel_",
+        "value": "2- Escriu el codi de registre a l'etiqueta.",
+        "description": "Book registered succesfully"
+    },
+    {
+        "key": "_PasteTheLabelInYourBook_",
+        "value": "3- Engantxa l'etiqueta al llibre",
+        "description": "Book registered succesfully"
+    },
+    {
+        "key": "_By_",
+        "value": "per",
+        "description": "title action by user"
+    },
+    {
+        "key": "_Map_",
+        "value": "Mapa",
+        "description": "Map button in the header"
+    },
+    {
+        "key": "_YourReview_",
+        "value": "La teva opiniÃ³",
+        "description": "Your review title of the review"
+    },
+    {
+        "key": "_Back_",
+        "value": "Enrrera",
+        "description": "Boton de atras"
+    },
+    {
+        "key": "_Reviews_",
+        "value": "Opinions",
+        "description": "Review title"
+    },
+    {
+        "key": "_Comments_",
+        "value": "Comentaris",
+        "description": "Comments title"
+    },
+    {
+        "key": "_Tracking_",
+        "value": "Mapa del viatge",
+        "description": "Tracking title"
+    },
+    {
+        "key": "_BookCrossingAction_",
+        "value": "Accions BookCrossing",
+        "description": "BookCrossing Actions title"
+    },
+    {
+        "key": "_Settings_",
+        "value": "Opcions",
+        "description": "Settings title"
+    },
+    {
+        "key": "_RegisterBook_",
+        "value": "Registra un llibre",
+        "description": "Register Book title"
+    },
+    {
+        "key": "_Scan_",
+        "value": "Escanejar",
+        "description": "Scan book"
+    },
+    {
+        "key": "_Detail_",
+        "value": "Detall",
+        "description": "Detail title"
+    },
+    {
+        "key": "_Library_",
+        "value": "Bilbioteca",
+        "description": "Library title"
+    },
+    {
+        "key": "_MyLibrary_",
+        "value": "La meva biblioteca",
+        "description": "Library title"
+    },
+    {
+        "key": "_List_",
+        "value": "Llista",
+        "description": "List Button"
+    },
+    {
+        "key": "_Hunt_",
+        "value": "CaÃ§a",
+        "description": "Hunt Button"
+    },
+    {
+        "key": "_FavoriteGenre_",
+        "value": "Genere preferit:",
+        "description": "My library favorite genre"
+    },
+    {
+        "key": "_WhatHappeningToday_",
+        "value": "Com va aixÃ²?",
+        "description": "What's happening today"
+    },
+    {
+        "key": "_Registers_",
+        "value": "Registres",
+        "description": "Registers in my library"
+    },
+    {
+        "key": "_Hunts_",
+        "value": "CaÃ§ats",
+        "description": "Hunt in my library"
+    },
+    {
+        "key": "_Comments_",
+        "value": "Comentaris",
+        "description": "Comments in my library"
+    },
+    {
+        "key": "_ListOfBooks_",
+        "value": "Llista de llibres",
+        "description": "List of books in my library"
+    },
+    {
+        "key": "_Status_",
+        "value": "Status:",
+        "description": "Status in my Library"
+    },
+    {
+        "key": "_Travel_",
+        "value": "Viatge:",
+        "description": "Number of kilometers to travel"
+    },
+    {
+        "key": "_Position_",
+        "value": "PosiciÃ³",
+        "description": "Position in the map"
+    },
+    {
+        "key": "_Route_",
+        "value": "Ruta",
+        "description": "Where the book has been to"
+    },
+    {
+        "key": "_Reviews_",
+        "value": "Opinions",
+        "description": "Number of reviews"
+    },
+    {
+        "key": "_Comments_",
+        "value": "comentaris",
+        "description": "Number of comments"
+    },
+    {
+        "key": "_Add_",
+        "value": "Afegir",
+        "description": "Add button in comments"
+    },
+    {
+        "key": "_AddYourComment_",
+        "value": "Afegeix el teu comentari",
+        "description": "Add your comment place holder"
+    },
+    {
+        "key": "_DateOfBirth_",
+        "value": "Data de naixement",
+        "description": "Add your comment place holder"
+    },
+    {
+        "key": "_NickName_",
+        "value": "Nom d'usuari",
+        "description": "Add your comment place holder"
+    }
+]
